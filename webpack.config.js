c16483be7cc9932dc342833bb357b3d5138e0830
Atabegovv <atabegov98@mail.ru>
2023-10-07 17:16:48 +0300

    security off temporarily

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const EslintWebpackPlugin = require('eslint-webpack-plugin');
-const StylelintWebpackPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -38,12 +36,6 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-        }),
-        new EslintWebpackPlugin({
-            files: '{**/*,*}.{js,jsx,ts,tsx}',
-        }),
-        new StylelintWebpackPlugin({
-            files: '{**/*,*}.{css,scss,sass}',
         }),
     ],
     devServer: {
